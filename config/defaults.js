9f4a0571a3ca1a85104c2d578af3eb14b22b12ca
heri16 <heri16@github.com>
2015-02-08 19:37:17 +0700

    Refactor to use PSRemoting

--- config/defaults.js
+++ config/defaults.js
@@ -36,12 +36,12 @@ exports.mailoutOptions = {
 exports.cpsReneOptions = {
   targets: {
     "3001": {
-      host: "30.139.40.15",
+      computerName: "30.139.40.15",
       username: "Mailin",
       passwordFile: "config\\3001.pem"
     },
     "3002": {
-      host: "30.188.136.245",
+      computerName: "30.188.136.245",
       username: "Mailin",
       passwordFile: "config\\3002.pem"
     }
@@ -51,14 +51,14 @@ exports.cpsReneOptions = {
 exports.cpsAccurateOptions = {
   targets: {
     "JAKARTA": {
-      host: "10.0.5.115",
+      computerName: "10.0.5.115",
       username: "Mailin",
       passwordFile: "config\\3001.pem",
       accurateDbHost: "10.0.4.10", 
       accurateGdbPath: "D:\\JAKARTA.GDB"
     },
     "BALI": {
-      host: "10.0.5.115",
+      computerName: "10.0.5.115",
       username: "Mailin",
       passwordFile: "config\\3001.pem",
       accurateDbHost: "10.0.4.10", 
